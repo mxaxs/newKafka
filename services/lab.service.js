d1a704d881ae65a23f73727aa063666f7fc5d3da
Marcio Santos <santos.marcioalex@gmail.com>
2022-09-14 12:05:07 -0300

    KAFKA Working 3

---
+++ services/lab.service.js
@@ -0,0 +1,9 @@
+const Laboratory = require("@moleculer/lab");
+
+module.exports = {
+	mixins: [Laboratory.AgentService],
+	settings: {
+		token: "3c330518-878a-43f9-847d-260865e494ff",
+		apiKey: "J7F0R1F-Q12MMJ5-Q2CRZ81-FCVQEJW"
+	}
+};
